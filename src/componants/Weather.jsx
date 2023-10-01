c4cea19a2f8c58ee7395b87f5b0d72fd9e64e2c7
tushar jain30 <tushar jain30>
2023-09-30 21:40:01 -1200

    first commit

---
+++ src/componants/Weather.jsx
@@ -0,0 +1,48 @@
+import React,{useState, useEffect} from 'react';
+import '../App.css';
+
+let Weather = () => {
+
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState('saharanpur');
+
+    let key = `7f296d69946509341261deebdba537ba`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${key}`;
+
+    useEffect(() => {
+        
+        const fetchData = async () => {
+            try{
+            let respone = await fetch(url);
+            let resJson = await respone.json();
+                setCity(resJson.main)
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+            
+        fetchData();
+    },[search]);
+        
+
+    return (
+        <>
+            <div className='box'>
+                <input type = 'text' className='input_type' placeholder='Enter your City' value = {search} onChange={(event) => {setSearch(event.target.value)}} />
+                { 
+                    !city ? (<p>Data Not Found, Please Enter City!</p>) : (
+                    <div>
+                    <h1 className='city_name'><i className="fa-solid fa-street-view"></i>{search}</h1>
+                    <div className='wave'>
+                        <h1 className="temp">{city.temp}°C</h1>
+                        <p className='temp-max-min'>Min : {city.temp_min}°C | Max : {city.temp_max}°C</p>
+                    </div>
+                    </div>
+                )}
+            </div>
+        </>    
+    )
+}
+
+export default Weather;
